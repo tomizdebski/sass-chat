a312cec6ca1b25fb413bf28121cf1788175fd31d
tomizdebski <tomizdebski@gmail.com>
2023-11-17 15:17:55 +0100

    add_theme_toggle

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,10 +1,27 @@
-
+import LogoImage from '@logos/black.svg'
+import Link from 'next/link'
+import { AspectRatio } from './ui/aspect-ratio'
+import Image from 'next/image'
 
 function Logo() {
   return (
-    <div>
-      Logo
-    </div>
+    <Link href='/' prefetch={false} className="overflow-hidden">
+      <div className='flex items-center w-72 h-14'>
+        <AspectRatio
+          ratio={16 / 9}
+          className='flex items-center justify-center'
+        >
+          <Image 
+          priority
+          src={LogoImage} 
+          alt='SASS Chat' 
+          className="dark:filter dark:invert"
+          height={40}
+          width={40}
+          />
+        </AspectRatio>
+      </div>
+    </Link>
   )
 }
 
---
+++ tailwind.config.js
@@ -0,0 +1,41 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
+  content: [
+    './pages/**/*.{js,jsx,mdx,ts,tsx}',
+    './components/**/*.{js,jsx,mdx,ts,tsx}',
+    './app/**/*.{js,jsx,mdx,ts,tsx}',
+    './src/**/*.{js,jsx,mdx,ts,tsx}',
+	],
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+}
